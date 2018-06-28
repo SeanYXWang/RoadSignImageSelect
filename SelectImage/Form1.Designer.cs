7d72c2699f5cc006d31fed445d05fc8f9993f721
Sean W <seanyongxiangwang@gmail.com>
2018-06-28 10:13:25 +0100

    First commit

---
+++ SelectImage/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SelectImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Target";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "NonTarget";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(199, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 563);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 138);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            809,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(184, 49);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 56);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Distraction";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1243, 612);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ SelectImage/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SelectImage
+{
+    public partial class Form1 : Form
+    {
+
+        string[] fileName = new String[1000];
+        bool file_read_in = false;
+        String path = @".\data\SelectedRoad2.txt";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if(File.Exists(@".\data\road2.txt")) // If a vote.csv file already exists then use it
+            {
+                read_in();
+                Display();
+            }
+            if (File.Exists(@".\data\SelectedRoad2.txt"))
+            {
+                File.Delete(@".\data\SelectedRoad2.txt");
+            }
+        }
+
+        // Set a variable to the My Documents path.
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int v = (int)numericUpDown1.Value;
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine(fileName[v]+" 1");
+            }
+            //pictureBox1.Image.Dispose();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void read_in()
+        {
+            using (StreamReader r = new StreamReader(@".\data\road2.txt"))
+            {
+                int i = 0;
+                string line;
+                while ((line = r.ReadLine()) != null)
+                {
+                    char[] whitespace = new char[] { ',' };   // Split line on ","
+                    string[] name = line.Split(whitespace);
+                    fileName[i] = name[0];
+                    i++;
+                }
+            }
+            file_read_in = true;
+        }
+
+        private void Display()
+        {
+            int v = (int)numericUpDown1.Value;
+            pictureBox1.Image = Image.FromFile(@".\road2\" + fileName[v]);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+           
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+                pictureBox1.Image.Dispose();
+            if (file_read_in)
+            {
+                Display();
+            }
+            else
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int v = (int)numericUpDown1.Value;
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine(fileName[v] + " 0");
+            }
+            //pictureBox1.Image.Dispose();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int v = (int)numericUpDown1.Value;
+            using (StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine(fileName[v] + " 2");
+            }
+        }
+    }
+}
